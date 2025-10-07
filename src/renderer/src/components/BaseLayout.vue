56a004bad55c3df77f3d668a742cc185aaa48411
TacKana <1@kiik.top>
2025-10-07 23:46:32 +0800

    调整布局组件栅格配置并移除topicCache页面中的BaseLayout组件

--- src/renderer/src/components/BaseLayout.vue
+++ src/renderer/src/components/BaseLayout.vue
@@ -3,10 +3,10 @@ import { ElRow, ElCol } from 'element-plus'
 </script>
 <template>
   <el-row :gutter="0">
-    <el-col :xs="4" :sm="4" :md="4" :lg="2" :xl="1">
+    <el-col :xs="6" :sm="4" :md="4" :lg="3" :xl="2">
       <slot name="list"></slot>
     </el-col>
-    <el-col :xs="20" :sm="20" :md="20" :lg="22" :xl="23">
+    <el-col :xs="18" :sm="20" :md="20" :lg="21" :xl="22">
       <slot name="body"></slot>
     </el-col>
   </el-row>
--- src/renderer/src/pages/topicCache.vue
+++ src/renderer/src/pages/topicCache.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Delete, Download, Edit, FolderAdd, Refresh } from '@element-plus/icons-vue'
-import { ElButton, ElInput, ElTable, ElTableColumn, ElText } from 'element-plus'
-import BaseLayout from '@renderer/components/BaseLayout.vue'
+import { ElButton, ElInput, ElTable, ElTableColumn /* ElText */ } from 'element-plus'
+// import BaseLayout from '@renderer/components/BaseLayout.vue'
 
 import { useWindowSize } from '@vueuse/core'
 const { height } = useWindowSize()
@@ -122,46 +122,46 @@ const tableData = [
 
 <template>
   <div class="topicCache">
-    <BaseLayout>
-      <template #list>
-        <div class="list">
+    <!-- <BaseLayout> -->
+    <!-- <template #list> -->
+    <!-- <div class="list">
           <ul>
             <li v-for="item in 6" :key="item">
               <el-text class="mx-1" size="large">默认题库{{ item }}</el-text>
             </li>
           </ul>
+        </div> -->
+    <!-- </template> -->
+    <!-- <template #body> -->
+    <div class="table">
+      <div class="operation">
+        <el-input style="width: 240px" placeholder="输入题目关键词" />
+        <div class="button">
+          <el-button type="danger" plain>清空</el-button>
+          <el-button type="primary" :icon="Edit" circle plain />
+          <el-button type="primary" :icon="FolderAdd" circle plain />
+          <el-button type="primary" :icon="Download" circle plain />
+          <el-button type="primary" :icon="Refresh" circle plain />
         </div>
-      </template>
-      <template #body>
-        <div class="table">
-          <div class="operation">
-            <el-input style="width: 240px" placeholder="输入题目关键词" />
-            <div class="button">
-              <el-button type="danger" plain>清空</el-button>
-              <el-button type="primary" :icon="Edit" circle plain />
-              <el-button type="primary" :icon="FolderAdd" circle plain />
-              <el-button type="primary" :icon="Download" circle plain />
-              <el-button type="primary" :icon="Refresh" circle plain />
-            </div>
-          </div>
-          <div class="body">
-            <el-table :data="tableData" stripe style="width: 100%" table-layout="fixed" :max-height="height - 100">
-              <el-table-column prop="date" label="问题" />
-              <el-table-column prop="name" label="答案" />
-              <el-table-column prop="address" label="操作" width="180">
-                <template #default="">
-                  <!-- <div>
+      </div>
+      <div class="body">
+        <el-table :data="tableData" stripe style="width: 100%" table-layout="fixed" :max-height="height - 100">
+          <el-table-column prop="date" label="问题" />
+          <el-table-column prop="name" label="答案" />
+          <el-table-column prop="address" label="操作" width="180">
+            <template #default="">
+              <!-- <div>
                     {{ scope.$index }}
                   </div> -->
-                  <el-button type="primary" :icon="Edit" circle plain />
-                  <el-button type="danger" :icon="Delete" circle plain />
-                </template>
-              </el-table-column>
-            </el-table>
-          </div>
-        </div>
-      </template>
-    </BaseLayout>
+              <el-button type="primary" :icon="Edit" circle plain />
+              <el-button type="danger" :icon="Delete" circle plain />
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </div>
+    <!-- </template> -->
+    <!-- </BaseLayout> -->
   </div>
 </template>
 
