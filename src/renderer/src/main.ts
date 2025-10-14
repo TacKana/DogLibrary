4a4c32c180ba104d54b9d3f519b0a0f61e4f4357
New XuXo <1@kiik.top>
2025-10-14 14:36:34 +0800

    设置ElementPlus和本地化配置，添加Clarity统计注释

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -5,10 +5,16 @@ import 'element-plus/theme-chalk/dark/css-vars.css' // 暗黑模式样式
 import './assets/iconfont/iconfont.js' //自定义图标
 import router from './router'
 import Clarity from '@microsoft/clarity'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 app.use(router)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
 
+// 配置微软clarity统计
 const projectId = 'tpstapzh7x'
 Clarity.init(projectId)
