c5bd521ca2880e267aef23415608373e467e7287
New XuXo <1@kiik.top>
2025-10-14 13:41:15 +0800

    feat: 添加Microsoft Clarity分析工具集成，更新CSP策略和安全配置

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -4,7 +4,11 @@ import './assets/global.scss'
 import 'element-plus/theme-chalk/dark/css-vars.css' // 暗黑模式样式
 import './assets/iconfont/iconfont.js' //自定义图标
 import router from './router'
+import Clarity from '@microsoft/clarity'
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+
+const projectId = 'tpstapzh7x'
+Clarity.init(projectId)
